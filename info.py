dbd2a09bbda69607e9f7939e3561515c78474145
Verma7890 <137143631+Verma7890@users.noreply.github.com>
2023-07-10 14:31:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filmycloud2:Verma7890@cluster0.cc25kzs.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://smnehal969797:nehalsingh969797@cluster0.4f8dn26.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Filmycloud1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
